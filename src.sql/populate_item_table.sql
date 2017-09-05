3c5b3bc9592143b09da49024cf7ca90f383b21d6
olehmv <oleg19850405@hotmail.com>
2017-09-05 11:05:04 +0300

    fourth commit

--- src.sql/populate_item_table.sql
+++ src.sql/populate_item_table.sql
@@ -4,3 +4,9 @@ Various porridges - vareniki filled with cottage cheese, potatoes, stewed cabbag
 Cabbage rolls stuffed with millet or minced meat with rice.', '50.00'), (NULL, '1', NULL, 'Ukrainian Deruny
 Potato fritters served with sour cream or cottage cheese.', '50.00'), (NULL, '1',NULL, 'Ukrainian Shashlyk 
 Type of shish kebab usually with marinated lamb and vegetables.', '70.00');
+INSERT INTO `item` (`id`, `menu_id`, `order_id`, `description`, `price`) VALUES (NULL, '2', NULL, 'Ukrainian borscht 
+Borsch is cooked of fresh vegetables: cabbage, beet, tomato with the addition of pounded lard with garlic and parsley. The combination of all these groceries gives the borscht its piquancy, aroma and unforgettable taste. There are about 30 types of Ukrainian borsch (Poltava borsch, Chernigov borsch, Kiev borsch, Volyn borsch, Lviv borsch and others).', '30.00'), (NULL, '2', NULL, 'Ukrainian vareniki
+Various porridges - vareniki filled with cottage cheese, potatoes, stewed cabbage and berries in summer - are prevalent in Ukraine too.', '40.00'), (NULL, '2', NULL, 'Ukrainian Holubtsi
+Cabbage rolls stuffed with millet or minced meat with rice.', '50.00'), (NULL, '2', NULL, 'Ukrainian Deruny
+Potato fritters served with sour cream or cottage cheese.', '50.00'), (NULL, '2',NULL, 'Ukrainian Shashlyk 
+Type of shish kebab usually with marinated lamb and vegetables.', '70.00');
